84932b8198e52ee44349c6fa0732ebfc22254c03
Jason Ng <1995jasonng@gmail.com>
2021-10-12 23:23:33 +1030

    name changed

--- react-first-dapp/contracts/NYYToken.sol
+++ react-first-dapp/contracts/NYYToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract NDToken is ERC20 {
+contract NYYToken is ERC20 {
     constructor() ERC20("NYY Token", "NYY") {
         _mint(msg.sender, 100000 * (10 ** 18));
     }
